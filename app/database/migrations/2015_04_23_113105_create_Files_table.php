6f20a7b4e1aafa480b46db1168c3295a33b33401
root <root@homestead>
2015-04-23 12:10:45 +0000

    Fixing composer related issues by adding vendor in .gitignore

---
+++ app/database/migrations/2015_04_23_113105_create_Files_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateFilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('Files', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('Files');
+	}
+
+}

ee565568ef389ddd84402acb4401dc49f310cb75
vagrant <vagrant@homestead>
2015-04-23 09:53:52 +0000

    Student as User for authentication fix

--- app/models/Student.php
+++ app/models/Student.php
@@ -1,6 +1,12 @@
 <?php
 
-class Student extends \Eloquent implements UserInterface, RemindableInterface  {
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface; 
+
+
+class Student extends \Eloquent implements UserInterface,RemindableInterface  {
 
 	use UserTrait, RemindableTrait;
 
@@ -11,5 +17,19 @@ class Student extends \Eloquent implements UserInterface, RemindableInterface  {
 
 	// Don't forget to fill this array
 	protected $fillable = [];
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'users';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password', 'remember_token');
 
 }

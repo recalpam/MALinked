9ca099a5910d4f42f0dc3f472c1913d17a21a749
Donny Hegener <admin@unleashgaming.net>
2015-05-20 04:15:41 +0000

    Nieuwe query methods! en nog meer dingetjes

--- app/views/js/controllers.js
+++ app/views/js/controllers.js
@@ -3,49 +3,7 @@
 * MaLinked Controllers
 *
 **/
-angular.module('MaLinked.Controllers', ['ngRoute'])
-
-/*==========  Home  ==========*/
-.controller('MainCtrl', ['$scope', '$state', '$stateParams', '$route', '$routeParams', '$location', '$timeout', 'ngProgress' ,function($scope, $state, $stateParams, $route, $routeParams, $location, $timeout, ngProgress){
-	
-
-
-    //80x80
-	$scope.thumbnail = function(url){
-		if(angular.isUndefined(url)){
-			return "/static/anon.jpg";
-		}else{
-			return url;
-		}
-	}
-
-	//250x250
-	$scope.medium = function(url){
-		if(angular.isUndefined(url)){
-			return "/static/anon.jpg";
-		}else{
-			return url;
-		}
-	}
-
-	//500x500
-	$scope.large = function(url){
-		if(angular.isUndefined(url)){
-			return "/static/anon.jpg";
-		}else{
-			return url;
-		}
-	}
-
-	//User uploaded size
-	$scope.original = function(url){
-		if(angular.isUndefined(url)){
-			return "/static/anon.jpg";
-		}else{
-			return url;
-		}
-	}
-}])
+angular.module('MaLinked.Controllers', [])
 
 /*==========  Home  ==========*/
 .controller('home', ['$scope', 'db', function($scope, db){
@@ -84,46 +42,19 @@ angular.module('MaLinked.Controllers', ['ngRoute'])
 	};
 
 	$scope.studies = htmlClasses;
-
-	$scope.getStudyClassFromSlug = function(slug){
-		var result = $filter('filter')(db.students, {slug: slug}, true);
-
-		if(result.length == 1){
-			result[0];
-		}
-
-	}
 }])
 
 /*==========  Profiel  ==========*/
 .controller('profiel', ['$scope', '$state', '$stateParams', '$filter', 'db', function($scope, $state, $stateParams, $filter, db){
-
-	$scope.boris = "hey";
-
-	// filter based upon id that we get from the url
-	var result = $filter('filter')(db.students, {slug: $stateParams.student}, true);
-
-	// if no results, we assume the user is trying to screw around with the app
-	if(result.length!=1){
-		// we will redirect it back to home
-		$state.go('home');
-	}
-
-	$scope.student = result[0];
-	console.log($scope.student);
-
-	
+	$scope.student = db.students.single({id: $stateParams.id});
 }])
 
 /*==========  Zoeken  ==========*/
 .controller('zoeken', ['$scope', 'db', function($scope, db){
-
 	$scope.db = db;
-
-
 }])
 
 /*==========  Beheer  ==========*/
-.controller('beheer', [function(){
+.controller('dev', [function(){
 	
 }])
--- app/views/js/services.js
+++ app/views/js/services.js
@@ -1,6 +1,31 @@
 /**
-*
-* MaLinked Services
-*
-**/
+ *
+ * MaLinked Services
+ *
+ **/
 angular.module('MaLinked.Services', [])
+    .directive('profielfoto', ['API',
+        function(API) {
+            var sync = API.sync();
+            return {
+                name: 'profielfoto',
+                template: '<img src="{{ uri }}" />',
+                controller: function($scope, $element, $attrs, $transclude) {
+                    $scope.uri = '';
+                    sync.then(function(db) {
+                    	if($attrs.studentId){
+                    		console.log(db.students.single({id: $attrs.studentId}));
+                    		/**
+                    		
+                    			TODO:
+                    			- img url @ uri 
+                    			- op basis van size attribute                    		
+                    		**/
+                    		
+                    	}
+                    });
+
+                }
+            };
+        }
+    ])

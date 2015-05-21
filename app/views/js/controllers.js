1db02bb2ac1894218dc1a8b1c187e5bf6d4c546b
Alex Lisenkov <contact@alexej.nl>
2015-05-21 11:39:42 +0200

    Homepage

--- app/views/js/controllers.js
+++ app/views/js/controllers.js
@@ -48,51 +48,114 @@ angular.module('MaLinked.Controllers', ['ngRoute'])
 }])
 
 /*==========  Home  ==========*/
-.controller('home', ['$scope', 'db', function($scope, db){
+.controller('home', ['$scope', '$filter', 'db', function($scope, $filter, db){
 	$scope.db = db;
 
+	var facts = {
+		0: 'sperma is dikker dan pis.',
+		1: 'er is meer water dan vis.',
+		2: 'dat weet je wat het is.',
+		3: 'een paard schijt meer dan een hagedis.'
+
+	};
+
+	var getRandomFact = function(){
+		var give;
+		var keys = Object.keys(facts);
+		var rand = keys[ keys.length * Math.random() << 0];
+
+		if( facts[rand] != null ){
+			var chosen = facts[rand];
+
+			give = {
+				'isFact': true,
+				'answer': chosen
+			}
+
+			facts[rand] = null;
+
+			console.log(keys);
+			return give;
+		}
+
+		return getRandomFact();
+		
+	}
+
+	var getStudyBySlug = function(slug){
+		var result = $filter('filter')(db.studies, {slug: slug}, true);
+		result[0]['isFact'] = false;
+
+		if(result.length == 1){
+			return result[0];
+		}
+	}
+
 	var htmlClasses = {
-		0 : {
-			'slug': 'grafisch-vormgever', 
-			'htmlClass': 'grafischvorgever'
+		'Aa' : {
+			'object': getStudyBySlug('grafisch-vormgever'), 
+			'htmlClass': 'grafischvormgeven'
 		},
-		1 : {
-			'slug': 'interactief-vormgever', 
+		'Ab' : {
+			'object': getStudyBySlug('interactief-vormgever'), 
 			'htmlClass': 'interactiefvormgeven'
 		},
-		2 : {
-			'slug': 'audiovisueel-vormgeveranimatie', 
+		'Ac' : {
+			'object': getStudyBySlug('audiovisueel-vormgeveranimatie'), 
 			'htmlClass': 'audiovisueel'
 		},
-		3: {
-			'slug': 'dtp-er', 
+		'Ad': {
+			'object': getStudyBySlug('dtp-er'), 
 			'htmlClass': 'dtper'
 		},
-		4: {
-			'slug': 'mediamanager',
+		'Ae': {
+			'object': getStudyBySlug('mediamanager'),
 			'htmlClass': 'mediamanager'
+		},
+		'Af': {
+			'object': getStudyBySlug('av-technicus'),
+			'htmlClass': 'audiovisueeltechnicus'
+		},
+		'Ag': {
+			'object': getStudyBySlug('evenemententechnicus'),
+			'htmlClass': 'evenemententechnicus'
+		},
+		'Ah': getRandomFact(),
+		'Ai': getRandomFact(),
+		'Aj': {
+			'object': getStudyBySlug('geluidstechnicus'),
+			'htmlClass': 'geluidtechnicus'
+		},
+		'Ak': {
+			'object': getStudyBySlug('media-en-gamedeveloper'),
+			'htmlClass': 'mediaengamedeveloper'
+		},
+		'Al': {
+			'object': getStudyBySlug('ruimtelijk-vormgever'),
+			'htmlClass': 'ruimtelijkvormgeven'
+		},
+		'Am': {
+			'object': getStudyBySlug('podiumtechnicus'),
+			'htmlClass': 'podiumtechnicus'
+		},
+		'An': {
+			'object': getStudyBySlug('filmacteur'),
+			'htmlClass': 'filmacteur'
+		},
+		'Ao': {
+			'object': getStudyBySlug('signmaker'),
+			'htmlClass': 'signmaker'
+		},
+		'Ap': {
+			'object': getStudyBySlug('game-artist'),
+			'htmlClass': 'gameartist'
 		}
-		//'av-technicus': 'audiovisueeltechnicus',
-		//'evenemententechnicus': 'evenemententechnicus',
-		//'geluidstechnicus': 'geluidtechnicus',
-		//'media-en-gamedeveloper': 'mediaengamedeveloper',
-		//'ruimtelijkvormgeven': 'ruimtelijkvormgeven',
-		//'podiumtechnicus': 'podiumtechnicus',
-		//'filmacteur': 'filmacteur',
-		//'signmaker': 'signmaker',
-		//'game-artist': 'gameartist'
 	};
 
 	$scope.studies = htmlClasses;
 
-	$scope.getStudyClassFromSlug = function(slug){
-		var result = $filter('filter')(db.students, {slug: slug}, true);
+	
 
-		if(result.length == 1){
-			result[0];
-		}
-
-	}
 }])
 
 /*==========  Profiel  ==========*/

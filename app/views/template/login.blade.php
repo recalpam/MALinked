65286c5e20c4c5ac2129aa4a7dd0a076d45a0619
Steven de Jong <stevendejnl@gmail.com>
2015-05-26 11:44:49 +0200

    Login deels

---
+++ app/views/template/login.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+	    <title>Ma linked Dashboard</title>
+
+	    {{-- OUTDATED BROWSER & JS --}}
+	    {{ HTML::script('/packages/modernizr/modernizr.js') }}
+	    {{ HTML::style('/packages/outdated-browser/outdatedbrowser/outdatedbrowser.min.css') }}
+
+		{{-- PAGE CSS --}}
+	    {{ HTML::style('/static/stylesheets/dashboard.css') }}
+
+	    <base href="/">
+		<style>
+			/* Styling for the ngProgress itself */
+			#ngProgress {
+			    margin: 0;
+			    padding: 0;
+			    z-index: 99998;
+			    background-color: green;
+			    color: green;
+			    box-shadow: 0 0 10px 0; /* Inherits the font color */
+			    height: 2px;
+			    opacity: 0;
+
+			    /* Add CSS3 styles for transition smoothing */
+			    -webkit-transition: all 0.5s ease-in-out;
+			    -moz-transition: all 0.5s ease-in-out;
+			    -o-transition: all 0.5s ease-in-out;
+			    transition: all 0.5s ease-in-out;
+			}
+
+			/* Styling for the ngProgress-container */
+			#ngProgress-container {
+			    position: fixed;
+			    margin: 0;
+			    padding: 0;
+			    top: 0;
+			    left: 0;
+			    right: 0;
+			    z-index: 99999;
+			}
+		</style>
+		<base href="/">
+	</head>
+	<body ng-app="MaLinked">
+
+		<!-- PAGE CONTAINER -->
+		<div class="site">
+
+			@include('template.login')
+
+			<!-- DYNAMIC PAGE -->
+			<ui-view></ui-view>
+		</div>
+
+		<!-- JQUERY TRASH -->
+		<script type="text/javascript" src="/packages/jquery/dist/jquery.js"></script>
+
+		<!-- ANGULARJS -->
+		<script type="text/javascript" src="/packages/angular/angular.js"></script>
+		<script type="text/javascript" src="/packages/angular-ui-router/release/angular-ui-router.js"></script>
+		<script type="text/javascript" src="/packages/angular-aria/angular-aria.js"></script>
+		<script type="text/javascript" src="/packages/angular-animate/angular-animate.js"></script>
+		<script type="text/javascript" src="/packages/angular-material/angular-material.js"></script>
+		<script type="text/javascript" src="/packages/angular-ngprogress/ngProgress.js"></script>
+
+		{{-- FOUNDATION --}}
+		<script src="/packages/foundation/js/foundation.min.js"></script>
+
+		{{-- ZOEK MODAL --}}
+		<script src="/static/scripts/app.js"></script>
+
+		{{-- OUTDATED BROWSER JS --}}
+		<script src="/packages/outdated-browser/outdatedbrowser/outdatedbrowser.min.js"></script>
+
+		<!-- MALINKED CLIENT -->
+		<script type="text/javascript" src="/api/frontend/file/script/progress"></script>
+		<script type="text/javascript" src="/api/frontend/file/script/controllers"></script>
+		<script type="text/javascript" src="/api/frontend/file/script/services"></script>
+		<script type="text/javascript" src="/api/frontend/file/script/factories"></script>
+		<script type="text/javascript" src="/api/frontend/file/script/routes"></script>
+		<script type="text/javascript" src="/api/frontend/file/script/app"></script>
+
+	</body>
+</html>
---
+++ public/static/scripts/dashboard.js
@@ -0,0 +1,54 @@
+/*==============================
+=            Global            =
+==============================*/
+
+
+/*==========  Browser Outdated  ==========*/
+//event listener: DOM ready
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function () {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+//call plugin function after DOM ready
+addLoadEvent(function () {
+  outdatedBrowser({
+    bgColor: '#f25648',
+    color: '#ffffff',
+    lowerThan: 'borderImage',
+    languagePath: '/static/components/outdated-browser/outdatedbrowser/lang/nl.html'
+  })
+});
+
+// Check IE Version
+function msieversion() {
+  var tmp = (document["documentMode"] || document.attachEvent) && "ev",
+    msie = tmp && (tmp = window[tmp + "al"]) && tmp("/*@cc_on 1;@*/") && +((/msie (d+)/i.exec(navigator.userAgent) || [])[1] || 0);
+  return msie || void 0;
+
+  console.log('test ' + tmp);
+};
+
+
+/*==========  Document Ready  ==========*/
+$(document).ready(function () {
+
+  // Bug fix fire on angular ready
+  setTimeout(function () {
+
+    // Foundation
+    $(document).foundation();
+
+    // Check IE version
+    msieversion();
+  }, 200);
+});
+/*-----  End of Global  ------*/

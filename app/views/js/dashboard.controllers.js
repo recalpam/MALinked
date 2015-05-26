d5a6f782c3210e3c1bc7759dada90116841f9a5b
Steven de Jong <stevendejnl@gmail.com>
2015-05-26 15:06:12 +0200

    Dashboard: Waarom het Ma?, Hobby's en gegevens deels

--- app/views/js/dashboard.controllers.js
+++ app/views/js/dashboard.controllers.js
@@ -15,9 +15,42 @@ angular.module('Dashboard.Controllers', [])
   }
 ])
 
-.controller('foo', [
+.controller('waarom-het-ma', ['$scope',
+  function ($scope) {
+    $scope.foo = "bar";
+  }
+])
 
-  function () {
+.controller('hobbys-en-gegevens', ['$scope',
+  function ($scope) {
+    $('.hobby-field input').tagsInput({
+      'interactive': true,
+      'height': '5.625rem',
+      'width': '100%',
+      'defaultText': 'voeg toe'
+    });
+  }
+])
 
+.controller('projecten', ['$scope',
+  function ($scope) {
+    $scope.foo = "bar";
+  }
+])
+
+.controller('over-de-school', ['$scope',
+  function ($scope) {
+    $('.docent-school-field input').tagsInput({
+      'interactive': true,
+      'height': '5.625rem',
+      'width': '100%',
+      'defaultText': 'voeg toe'
+    });
+  }
+])
+
+.controller('toekomstbeeld', ['$scope',
+  function ($scope) {
+    $scope.foo = "bar";
   }
 ])

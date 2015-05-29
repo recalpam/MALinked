bd826664f7449e370e7e9034ad81504544055721
Steven de Jong <stevendejnl@gmail.com>
2015-05-29 11:45:45 +0200

    Home Header Video

--- app/views/template/header.blade.php
+++ app/views/template/header.blade.php
@@ -9,7 +9,7 @@
 				<span class="text">Ma Linked</span>
 			</div>
 			<ul>
-				<a ui-sref="opleidingen"><li>Opleidingen</li></a>
+				<a ui-sref="#" ng-click="goToOpleidingen()"><li>Opleidingen</li></a>
 				<a href="#" data-reveal-id="searchModal"><li>Zoeken</li></a>
 			</ul>
 		</div>
@@ -26,11 +26,13 @@
 				Sorry, your browser does not support HTML5 video.
 			</video>
 			<div class="content">
-				<div class="search-bar row">
-					<input name="search-bar" placeholder="Zoek direct op een opleiding, klas of student" onfocus="this.placeholder=''" onblur="this.placeholder='Zoek direct op een opleiding, klas of student'">
-				</div>
+				<a href="#" data-reveal-id="searchModal">
+					<div class="search-bar row">
+						<input name="search-bar" placeholder="Zoek direct op een opleiding, klas of student" onfocus="this.placeholder=''" onblur="this.placeholder='Zoek direct op een opleiding, klas of student'">
+					</div>
+				</a>
 			</div>
-			<div class="overlay" style="background-image: url(/static/images/video-background.png);"></div>
+			<div class="overlay"></div>
 		</angular>
 
 		{{-- PROFIEL LAYOUT --}}
@@ -56,6 +58,23 @@
 
 			</div>
 			<div class="overlay"></div>
+		</angular>
+
+		{{-- OPLEIDING LAYOUT --}}
+		<angular ng-switch-when="opleiding">
+      <div class="content">
+        <div class="align">
+          <div class="row">
+            <div class="small-12 columns">
+              <h1>Grafisch Vormgeven 4A</h1>
+            </div>
+            <div class="small-12 columns">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat in voluptate non proident.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="overlay"></div>
 		</angular>
 
 	{{-- END SWITCH --}}

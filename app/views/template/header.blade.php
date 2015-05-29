0eadbcbabfbdefce5ba3869a524c7da43fbce9d4
Steven de Jong <stevendejnl@gmail.com>
2015-05-29 16:59:04 +0200

    Header Images, Klassen Foto's en Opleiding Foto's

--- app/views/template/header.blade.php
+++ app/views/template/header.blade.php
@@ -1,5 +1,5 @@
 {{-- HEADER START --}}
-<header class="@{{ state.current.name }}" ng-class="@{{ headerClass }}">
+<header class="@{{ state.current.name }} @{{ headerClass }}" ng-class="@{{ headerClass }}">
 
 	{{-- NAV --}}
 	<nav class="menu" role="navigation">
@@ -34,7 +34,7 @@
 			</div>
 			<div class="overlay"></div>
 		</angular>
-	
+
 		{{-- PROFIEL LAYOUT --}}
 		<angular ng-switch-when="profiel">
 			<div class="content">
@@ -69,7 +69,7 @@
               <h1>@{{ study.name }}</h1>
             </div>
             <div class="small-12 columns">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat in voluptate non proident.</p>
+              {{-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat in voluptate non proident.</p> --}}
             </div>
           </div>
         </div>

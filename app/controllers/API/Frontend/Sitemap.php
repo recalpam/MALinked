148088eee9710d2faa39ca3a3dbdea38c08f367d
Alex Lisenkov <contact@alexej.nl>
2015-06-22 01:24:33 +0200

    Changed priorities

--- app/controllers/API/Frontend/Sitemap.php
+++ app/controllers/API/Frontend/Sitemap.php
@@ -16,17 +16,17 @@ class Sitemap extends \BaseController {
 
 	// Do the students
 	foreach($data->students as $student){
-		$sitemap->add(URL::to('/profiel/'. $student->group->slug .'/'. $student->slug), $student->info->updated_at, '0.8', 'daily');
+		$sitemap->add(URL::to('/profiel/'. $student->group->slug .'/'. $student->slug), $student->info->updated_at, '0.6', 'daily');
 	}
 
 	// Do the studies
 	foreach($data->studies as $study){
-		$sitemap->add(URL::to('/opleiding/'. $study->slug), $study->updated_at, '0.8', 'monthly');
+		$sitemap->add(URL::to('/opleiding/'. $study->slug), $study->updated_at, '0.9', 'monthly');
 	}
 
 	// Do the groups
 	foreach($data->groups as $group){
-		$sitemap->add(URL::to('/klas/'. $group->slug), $group->updated_at, '0.8', 'monthly');
+		$sitemap->add(URL::to('/klas/'. $group->slug), $group->updated_at, '0.7', 'monthly');
 	}
 
     return $sitemap->render('xml');
